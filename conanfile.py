422bfe4797ffec234f73271f3084736a103d9ed9
Uilian Ries <uilianries@gmail.com>
2017-11-22 19:51:21 -0200

    Build on Linux
    - Adapted to support Linux build
    - Should works on Mac and Windows - Not tested
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
 import os
+import tempfile
+from conans import ConanFile, CMake, tools
+
 
 class OpenJpegConan(ConanFile):
     name = "openjpeg"
     version = "2.3.0"
     description = "OpenJPEG is an open-source JPEG 2000 codec written in C language."
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    generators = "cmake"
+    options = {"shared": [True, False], "build_codec": [True, False]}
+    default_options = "shared=False", "build_codec=True"
     settings = "os", "compiler", "build_type", "arch"
-    exports = ["CMakeLists.txt", "LICENSE"]
-    url="https://github.com/bincrafters/conan-openjpeg"
-    license="https://github.com/uclouvain/openjpeg/blob/master/LICENSE"
+    generators = "cmake"
+    exports = "LICENSE"
+    exports_sources = "CMakeLists.txt"
+    url = "https://github.com/bincrafters/conan-openjpeg"
+    license = "https://github.com/uclouvain/openjpeg/blob/master/LICENSE"
+    author = "Alexander Zaitsev <zamazan4ik@tut.by>"
+    requires = "zlib/1.2.11@conan/stable"
+    install_dir = tempfile.mkdtemp(name)
 
     def source(self):
         source_url = "https://github.com/uclouvain/openjpeg"
@@ -24,17 +30,35 @@ class OpenJpegConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.install_dir
         cmake.configure()
         cmake.build()
+        cmake.install()
 
     def package(self):
-        dst_folder=self.name + '-2.3'
-        self.copy("*.h", dst="include/openjpeg-2.3", keep_path=False)
-        self.copy("*.lib", dst="lib", src="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", src="sources", keep_path=False)
-        self.copy("*.dylib", dst="lib", src="sources", keep_path=False)
-        self.copy("*.so*", dst="lib", src="sources", keep_path=False)
-        self.copy("*.a", dst="lib", src="lib", keep_path=False)
+        lib_dir = os.path.join(self.install_dir, "lib")
+        bin_dir = os.path.join(self.install_dir, "bin")
+        self.copy(pattern="LICENSE", dst=".", src=os.path.join("source", "LICENSE"))
+        self.copy("*.h", dst="include", src=os.path.join(self.install_dir, "include"))
+        self.copy("*.cmake", dst="res", src=lib_dir, keep_path=False)
+        if self.options.build_codec:
+            self.copy("opj_*", dst="bin", src=bin_dir, keep_path=False)
+        if self.settings.os == "Windows":
+            if self.options.shared:
+                self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+            self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+        elif str(self.settings.os) in ['Linux', 'Android']:
+            if self.options.shared:
+                self.copy("*.so*", dst="lib", src="lib", keep_path=False)
+            else:
+                self.copy("*.a", dst="lib", src="lib", keep_path=False)
+        elif str(self.settings.os) in ['Macos', 'iOS', 'watchOS', 'tvOS']:
+            if self.options.shared:
+                self.copy("*.dylib", dst="lib", src="lib", keep_path=False)
+            else:
+                self.copy("*.a", dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["openjp2"]
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
