defc20c3c47ea2076a43570938b6f65776af4ff9
theirix <theirix@gmail.com>
2018-10-20 10:13:18 +0300

    Updated Conan conventions

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class OpenjpegConan(ConanFile):
     version = "2.3.0"
     description = "OpenJPEG is an open-source JPEG 2000 codec written in C language."
     options = {"shared": [True, False], "build_codec": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "build_codec=True", "fPIC=True"
+    default_options = {'shared': False, 'build_codec': True, 'fPIC': True}
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     exports = "LICENSE.md"
@@ -20,7 +20,7 @@ class OpenjpegConan(ConanFile):
     homepage = "https://github.com/uclouvain/openjpeg"
     license = "BSD 2-Clause"
 
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
 
     def requirements(self):
         self.requires.add("zlib/1.2.11@conan/stable")
@@ -36,19 +36,19 @@ class OpenjpegConan(ConanFile):
         source_url = "https://github.com/uclouvain/openjpeg"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
-        os.rename(os.path.join(self.source_subfolder, "CMakeLists.txt"),
-                  os.path.join(self.source_subfolder, "CMakeListsOriginal.txt"))
+        os.rename(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                  os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"))
         shutil.copy("CMakeLists.txt",
-                    os.path.join(self.source_subfolder, "CMakeLists.txt"))
+                    os.path.join(self._source_subfolder, "CMakeLists.txt"))
 
     def build(self):
         # ensure our lcms is used
-        os.unlink(os.path.join(self.source_subfolder, 'cmake', 'FindLCMS2.cmake'))
+        os.unlink(os.path.join(self._source_subfolder, 'cmake', 'FindLCMS2.cmake'))
 
         # fix installing libs when only shared or static library built
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'src', 'lib', 'openjp2', 'CMakeLists.txt'),
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'src', 'lib', 'openjp2', 'CMakeLists.txt'),
                               'add_library(${OPENJPEG_LIBRARY_NAME} ${OPENJPEG_SRCS})',
                               'add_library(${OPENJPEG_LIBRARY_NAME} ${OPENJPEG_SRCS})\n'
                               'set(INSTALL_LIBS ${OPENJPEG_LIBRARY_NAME})')
@@ -59,12 +59,12 @@ class OpenjpegConan(ConanFile):
         cmake.definitions['BUILD_PKGCONFIG_FILES'] = True
         if self.settings.os != "Windows":
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
-        cmake.configure(source_folder=self.source_subfolder)
+        cmake.configure(source_folder=self._source_subfolder)
         cmake.build()
         cmake.install()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         # remove binaries
         for bin_program in ['opj_dump', 'opj_compress', 'opj_decompress']:
             for ext in ['', '.exe']:
