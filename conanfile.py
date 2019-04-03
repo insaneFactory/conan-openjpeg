4239d8ca670efca91d2f387215338a57bb13e2ad
theirix <theirix@gmail.com>
2019-04-03 18:34:58 +0300

    Update version 2.3.1

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from conans import ConanFile, CMake, tools
 
 class OpenjpegConan(ConanFile):
     name = "openjpeg"
-    version = "2.3.0"
+    version = "2.3.1"
     description = "OpenJPEG is an open-source JPEG 2000 codec written in C language."
     options = {"shared": [True, False], "build_codec": [True, False], "fPIC": [True, False]}
     default_options = {'shared': False, 'build_codec': True, 'fPIC': True}
